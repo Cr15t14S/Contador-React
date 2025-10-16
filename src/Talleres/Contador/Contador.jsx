db741bcbd1ea46abc12db7b10164b79d105acc08
Cristian <mao.salgado7@gmail.com>
2025-10-16 11:26:09 -0500

    useEffect

--- src/Talleres/Contador/Contador.jsx
+++ src/Talleres/Contador/Contador.jsx
@@ -1,21 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import styles from "./styles.module.css";
 
 const Contador = () => {
   const [contador, setContador] = useState(0);
   const handleClick = () => {
     setContador(contador + 1);
+    setNombre("xd");
   };
   const handleDisminuir = () => {
     setContador(contador - 1);
   };
+
+  const [color, setColor] = useState("red");
+  const [nombre, setNombre] = useState("");
+  
+  const handleColorChange = () => {
+    setColor("Azul");
+  };
+
+  useEffect(() => {
+    document.title = `Contador: ${contador} y color: ${color}`;
+  }, [contador, color]);
+
   return (
     <div className={styles.cardContador}>
       <button className={styles.button} onClick={handleClick}>
         Aumentar
       </button>
       <div className={styles.value}>{contador}</div>
-      <button className={styles.button} onClick={handleDisminuir}>
+      <button
+        className={styles.button}
+        onClick={() => {
+          handleDisminuir();
+          handleColorChange();
+      }}
+      >
         Disminuir
       </button>
     </div>
